9a04046c0f242ad7e735a99144eec7a284a6dc28
Andrew Parisi <andrew.p.parisi@gmail.com>
2021-11-28 09:16:29 -0500

    master
    Tried to use spec for formula but it didn't work

--- src/clj/logos/formula.clj
+++ src/clj/logos/formula.clj
@@ -1,4 +1,62 @@
-(ns logos.formula)
+(ns logos.formula
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.spec.gen.alpha :as gen]
+            ))
+
+(spec/def ::constant (spec/and string? #(not (= % ""))))
+(spec/def ::variable symbol?)
+
+(spec/def ::term (spec/or :constant ::constant :var ::variable))
+
+(spec/def ::arguments (spec/coll-of
+                       ::term :kind vector?))
+
+(spec/def ::predicate (spec/or :atomic string? :compound ::lambda))
+
+(spec/def ::atom
+  (spec/keys :req [::atomic-predicate ::arguments]))
+
+(spec/def ::conjuncts (spec/coll-of ::formula))
+(spec/def ::conjunction
+  (spec/keys :req [::conjuncts]))
+
+
+(spec/def ::disjuncts (spec/coll-of ::formula))
+(spec/def ::disjunction
+  (spec/keys :req [::disjuncts]))
+
+(spec/def ::antecedent ::formula)
+(spec/def ::consequent ::formula)
+(spec/def ::implication
+  (spec/keys :req [::antecendent ::consequent]))
+
+
+(spec/def ::negatum ::formula)
+(spec/def ::negation (spec/keys :req [::negatum]))
+
+
+(spec/def ::bound-formula ::formula)
+
+(spec/def ::universal-vars (spec/coll-of ::variable))
+(spec/def ::universal
+  (spec/keys :req [::universal-vars ::bound-formula]))
+
+(spec/def ::existential-vars (spec/coll-of ::variable))
+(spec/def ::existential
+  (spec/keys :req [:existential-vars ::bound-formula]))
+
+(spec/def ::lambda-vars (spec/coll-of ::variable))
+(spec/def ::lambda
+  (spec/keys :req [::lambda-vars ::formula]))
+
+(spec/def ::formula (spec/or :atom ::atom
+                             :conjunction ::conjunction
+                             :disjunction ::disjunction
+                             :implication ::implication
+                             :negation ::negation
+                             :universal ::universal
+                             :existential ::existential))
+
 
 (declare formula?)
 
