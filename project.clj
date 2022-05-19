b4ee9b9acf3174702f6288f0b8ddfda259539429
Andrew Parisi <andrew.p.parisi@gmail.com>
2022-05-18 21:55:43 -0400

    develop: Version Bump [skip ci]
    Version bump from 1 to 2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject logos "1.0.0"
+(defproject logos "2.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
